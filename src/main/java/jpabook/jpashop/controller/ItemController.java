11a07d243b68b2cc80a7e2059ba49cfd94d66421
lyae <lyaesley@gmail.com>
2023-06-30 00:03:12 +0900

    변경 감지와 병합(merge)

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -80,8 +80,7 @@ public class ItemController {
      * 상품 수정, 권장 코드
      */
     @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form")
-    BookForm form) {
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
         itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
